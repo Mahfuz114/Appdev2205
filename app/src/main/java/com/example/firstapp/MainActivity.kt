63e8f5e2477f8e0520fb11837372e3d3e5f0c58c
Adnan <mahfuzalam37a@gmail.com>
2023-02-27 21:01:43 +0600

    Initial Commit

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,11 +1,29 @@
-package com.example.firstapp
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set up the button click listener
+        calculateButton.setOnClickListener {
+            calculateBMI()
+        }
+    }
+
+    private fun calculateBMI() {
+        // Get the user's input from the EditText fields
+        val weight = weightEditText.text.toString().toDouble()
+        val height = heightEditText.text.toString().toDouble()
+
+        // Calculate the BMI
+        val bmi = weight / (height * height)
+
+        // Display the result
+        resultTextView.text = "Your BMI is ${"%.2f".format(bmi)}"
     }
 }

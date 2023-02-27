d448c97af74912bec612f9ad2197a88028e8dd42
Adnan <mahfuzalam37a@gmail.com>
2023-02-27 21:10:46 +0600

    Initial Commit

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,6 +1,5 @@
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
